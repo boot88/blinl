e591b06b507152ba3a12e682876e1416c7258a0e
Daniel Schreck <daniel.s.schreck@gmail.com>
2022-03-17 03:44:05 -0400

    more pipi in pampers

--- blink.ino
+++ blink.ino
@@ -22,4 +22,5 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
+  pipiinmypampersagain();  // make more pepe in pampers
 }
